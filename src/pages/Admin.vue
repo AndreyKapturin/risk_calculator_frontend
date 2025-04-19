015a4fc7d30384c2f5939e5060402d911e37babd
Andrey Kapturin <andruxa21097@gmail.com>
2025-04-19 10:51:56 +0700

    feat: Форма добавления метрики. Вывод списка метрик. Редактирование метрики и индикаторов

---
+++ src/pages/Admin.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="admin-page">
+    <nav class="sidebar">
+      <RouterLink to="/admin/metrics">Метрики</RouterLink>
+      <RouterLink to="/admin/create-metric">Создать метрику</RouterLink>
+    </nav>
+    <RouterView />
+  </section>
+</template>
+
+<style scoped>
+  .admin-page {
+    display: flex;
+    gap: 20px;
+  }
+
+  .sidebar {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,28 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import Main from '../pages/Main.vue';
 import Form from '../pages/Form.vue';
+import Admin from '../pages/Admin.vue';
+import CreateMetricForm from '../components/CreateMetricForm.vue';
+import MetricsList from '../components/MetricsList.vue';
+import MetricCard from '../components/MetricCard.vue';
 
 const routes = [
   { path: '/', component: Main },
   { path: '/objects-groups/:id', component: Form },
+  { path: '/admin', component: Admin, children: [
+    {
+      path: 'metrics',
+      component: MetricsList
+    },
+    {
+      path: 'metrics/:id',
+      component: MetricCard
+    },
+    {
+      path: 'create-metric',
+      component: CreateMetricForm
+    }
+  ]},
 ]
 
 const router = createRouter({
