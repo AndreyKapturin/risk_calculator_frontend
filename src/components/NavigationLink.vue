0f02567bff044d116c1f757c7b6d19262d098753
Andrey Kapturin <andruxa21097@gmail.com>
2025-04-21 07:54:21 +0700

    feat: В сайдбар добавлены активные ссылки

---
+++ src/components/NavigationLink.vue
@@ -0,0 +1,21 @@
+<script setup>
+  defineProps(['to']);
+</script>
+
+<template>
+  <RouterLink class="navigation-link" :to="to"><slot/></RouterLink>
+</template>
+
+<style scoped>
+  .navigation-link {
+    text-decoration: none;
+    color: var(--text-color);
+    padding: 10px;
+    border-radius: var(--border-radius);
+    background-color: var(--primary-color);
+  }
+  .navigation-link:hover,
+  .navigation-link.router-link-active {
+    background-color: var(--secondary-color);
+  }
+</style>
