3e789bd9cd8c5e44ed9c634ec61580969accc739
Andrey Kapturin <andruxa21097@gmail.com>
2025-04-24 06:30:18 +0700

    feat: Форма редактирования метрики
    - Заменены кнопки
    - Добавлено состояние измененности и блокирование отправки форм без изменений

--- src/components/EditMetricForm.vue
+++ src/components/EditMetricForm.vue
@@ -1,35 +1,65 @@
 <script setup>
-import { ref } from 'vue';
-import { updateMetric } from '../api.js';
+  import { computed, ref } from 'vue';
+  import { updateMetric } from '../api.js';
+  import Textarea from '../ui/Textarea.vue';
+  import { METRIC_TYPES, METRIC_TYPES_TRANSLATE } from '../constants.js';
+  import FormErrorMessage from '../ui/FormErrorMessage.vue';
+  import Joi from 'joi';
 
-const props = defineProps(['metric']);
-const emit = defineEmits((['updateMetric']))
+  const props = defineProps(['metric']);
+  const emit = defineEmits((['updateMetric']))
 
-const metricData = ref({
-  name: props.metric.name,
-  type: props.metric.type,
-});
+  const metricData = ref({
+    name: props.metric.name,
+    type: props.metric.type,
+  });
 
-const onSubmitForm = async () => {
-  try {
-    const updatedMetric = await updateMetric(props.metric.id, metricData.value);
-    emit('updateMetric', updatedMetric);
-  } catch (error) {
-    console.log(error);
+  const isEdited = computed(() => (metricData.value.name !== props.metric.name) || (metricData.value.type !== props.metric.type))
+
+  const formErrors = ref(null);
+  const formValidateScheme = Joi.object(
+    {
+      name: Joi.string().min(1).max(1000).messages({
+        'string.empty': 'Текст метрики не может быть пустым',
+        'string.max': 'Текст метрики не может быть больше 1000 символов',
+      }),
+      type: Joi.valid(METRIC_TYPES.RISK_INDICATOR, METRIC_TYPES.GOOD_FAITH_CRITERIA),
+    }
+  );
+
+  const onSubmitForm = async () => {
+    const { value, error } = formValidateScheme.validate(metricData.value);
+    formErrors.value = null;
+
+    if (error !== undefined) {
+      formErrors.value = error.details[0].message;
+      return;
+    }
+    
+    try {
+      const updatedMetric = await updateMetric(props.metric.id, value);
+      emit('updateMetric', updatedMetric);
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
 </script>
 
 <template>
   <form class="update-metric-form">
-    <label for="metric-name">Текст метрики:</label>
-    <textarea class="metric-name-textarea" id="metric-name" v-model="metricData.name"></textarea>
-    <label for="metric-type">Тип метрики:</label>
-    <select id="metric-type" v-model="metricData.type">
-      <option value="risk indicator">Индикатор риска</option>
-      <option value="good faith criteria">Критерий добросовестности</option>
-    </select>
-    <button type="button" @click="onSubmitForm">Сохранить</button>
+    <Label>
+      Текст метрики:
+      <Textarea v-model="metricData.name" rows="3" />
+    </Label>
+    <Label>
+      Тип метрики:
+      <Select v-model="metricData.type">
+        <option :value="METRIC_TYPES.RISK_INDICATOR">{{ METRIC_TYPES_TRANSLATE[METRIC_TYPES.RISK_INDICATOR] }}</option>
+        <option :value="METRIC_TYPES.GOOD_FAITH_CRITERIA">{{ METRIC_TYPES_TRANSLATE[METRIC_TYPES.GOOD_FAITH_CRITERIA] }}</option>
+      </Select>
+    </Label>
+    <FormErrorMessage v-if="formErrors">{{ formErrors }}</FormErrorMessage>
+    <Button :disabled="!isEdited" type="button" @click="onSubmitForm">Сохранить</Button>
   </form>
 </template>
 
