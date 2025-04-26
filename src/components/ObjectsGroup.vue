9b19dc5d159df7c161363fa1ab66d494d403c381
Andrey Kapturin <andruxa21097@gmail.com>
2025-04-26 13:21:43 +0700

    feat: Редактирование данных о группе объектов
    - Форма и запрос для изменения названия и показателей

--- src/components/ObjectsGroup.vue
+++ src/components/ObjectsGroup.vue
@@ -3,24 +3,54 @@
   import { useLoadData } from '../hooks/useLoadData.js';
   import { getObjectsGroupById } from '../api.js';
   import { METRIC_TYPES } from '../constants.js';
-  import { computed } from 'vue';
+  import { computed, ref } from 'vue';
+  import EditObjectsGroupForm from './EditObjectsGroupForm.vue';
 
   const route = useRoute();
   const objectsGroupId = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
   const { data: objectsGroup, isLoading } = useLoadData(() => getObjectsGroupById(objectsGroupId));
+  
   const riskIndicators = computed(() => objectsGroup.value?.metrics?.filter(m => m.type === METRIC_TYPES.RISK_INDICATOR));
   const goodFaithCriteries = computed(() => objectsGroup.value?.metrics?.filter(m => m.type === METRIC_TYPES.GOOD_FAITH_CRITERIA));
+  const isEditMode = ref(false);
+  const setEditMode = () => isEditMode.value = true;
+  const cancelEditMode = () => isEditMode.value = false;
+  const handleDeleteObjectsGroup = () => {
+    console.log('Delete objects group');
+  }
+
+  const onUpdateObjectsGroup = (updatedObjectsGroup) => {
+    objectsGroup.value = updatedObjectsGroup;
+  }
 
 </script>
 
 <template>
   <Loading v-if="isLoading" />
   <article v-else class="objects-group">
-    <h1>{{ objectsGroup.name }}</h1>
-    <p>Показатель тяжести социальных последствий пожара:</p>
-    <p>{{ objectsGroup.socialDamagePotencialScore }}</p>
-    <p>Показатель тяжести материальных последствий пожара:</p>
-    <p>{{ objectsGroup.materialDamagePotencialScore }}</p>
+    <div class="objects-group__header">
+      <h1>Группа объектов</h1>
+      
+      <div class="control-buttons">
+        <template v-if="isEditMode">
+          <Button @click="cancelEditMode">Закрыть</Button>
+        </template>
+        <template v-else>
+          <Button @click="setEditMode">Изменить</Button>
+          <Button @click="handleDeleteObjectsGroup">Удалить</Button>
+        </template>
+      </div>
+    </div>
+
+    <EditObjectsGroupForm v-if="isEditMode" :objectsGroup @updateObjectsGroup="onUpdateObjectsGroup"/>
+    <template v-else>
+      <h4>Название группы объектов:</h4>
+      <p>{{ objectsGroup.name }}</p>
+      <h4>Показатель тяжести социальных последствий пожара:</h4>
+      <p>{{ objectsGroup.socialDamagePotencialScore }}</p>
+      <h4>Показатель тяжести материальных последствий пожара:</h4>
+      <p>{{ objectsGroup.materialDamagePotencialScore }}</p>
+    </template>
 
     <h2>Метрики:</h2>
 
@@ -54,13 +84,20 @@
     flex-direction: column;
     gap: 10px;
   }
-
+  .objects-group__header {
+    display: flex;
+    justify-content: space-between;
+  }
   .metric__name {
     border-bottom: 1px solid rgb(128, 128, 128);
   }
-
   .indicator {
     display: flex;
     justify-content: space-between;
+  }
+  .control-buttons {
+    display: flex;
+    justify-content: end;
+    gap: 10px;
   }
 </style>
